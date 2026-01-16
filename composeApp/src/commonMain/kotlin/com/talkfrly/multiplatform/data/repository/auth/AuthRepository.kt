5ce2cc07b69e132e6b4b67d37f2f0e35d24379bd
michaelarchman <mike.projektowanie@gmail.com>
2026-01-16 20:13:41 +0100

    feat: auth repository created

--- composeApp/src/commonMain/kotlin/com/talkfrly/multiplatform/data/repository/auth/AuthRepository.kt
+++ composeApp/src/commonMain/kotlin/com/talkfrly/multiplatform/data/repository/auth/AuthRepository.kt
@@ -1,4 +1,40 @@
 package com.talkfrly.multiplatform.data.repository.auth
 
-class AuthRepository {
+import com.talkfrly.multiplatform.data.api.AuthApi
+import com.talkfrly.multiplatform.data.mapper.toDomain
+import com.talkfrly.multiplatform.domain.core.DataResult
+import com.talkfrly.multiplatform.domain.core.map
+import com.talkfrly.multiplatform.domain.models.DataError
+import com.talkfrly.multiplatform.domain.models.LoginRequest
+import com.talkfrly.multiplatform.domain.models.LoginResponse
+import com.talkfrly.multiplatform.domain.models.RegisterRequest
+import com.talkfrly.multiplatform.domain.models.RegisterResponse
+import com.talkfrly.multiplatform.domain.models.User
+
+interface AuthRepository  {
+    suspend fun login(loginRequest: LoginRequest): DataResult<LoginResponse, DataError.Remote>
+    suspend fun register(registerRequest: RegisterRequest): DataResult<RegisterResponse, DataError.Remote>
+    suspend fun getCurrentUser(): DataResult<User, DataError.Remote>
+}
+
+class AuthRepositoryImpl(
+    private val api: AuthApi
+): AuthRepository {
+    override suspend fun login(loginRequest: LoginRequest): DataResult<LoginResponse, DataError.Remote> {
+        return api
+            .login(loginRequest)
+            .map { it.toDomain() }
+    }
+
+    override suspend fun register(registerRequest: RegisterRequest): DataResult<RegisterResponse, DataError.Remote> {
+        return api
+            .register(registerRequest)
+            .map { it.toDomain() }
+    }
+
+    override suspend fun getCurrentUser(): DataResult<User, DataError.Remote> {
+        return api
+            .getCurrentUser()
+            .map { it.toDomain() }
+    }
 }
