dc9b38f07d95cac966150c2158b1f4ee3a0528e4
michaelarchman <mike.projektowanie@gmail.com>
2026-01-25 10:51:08 +0100

    refactor: cleansing imports

--- composeApp/src/commonMain/kotlin/com/talkfrly/multiplatform/ui/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/talkfrly/multiplatform/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.talkfrly.multiplatform.ui.screens.home
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
@@ -10,10 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,8 +23,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.painter.BrushPainter
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.talkfrly.multiplatform.ui.Route
