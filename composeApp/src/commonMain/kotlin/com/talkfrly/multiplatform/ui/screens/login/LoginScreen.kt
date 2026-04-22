a344ca4d10673584961ca21d370cbbc97142ca9b
michaelarchman <mike.projektowanie@gmail.com>
2026-04-22 15:38:40 +0200

    refactor: cleaning imports

--- composeApp/src/commonMain/kotlin/com/talkfrly/multiplatform/ui/screens/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/talkfrly/multiplatform/ui/screens/login/LoginScreen.kt
@@ -12,13 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
